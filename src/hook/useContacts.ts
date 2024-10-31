61f574cc8d4878fa729401b72ac66d34bd9ad66e
Tamil0105 <tamil16399@gmail.com>
2024-10-31 18:22:27 +0530

    build issue fix

--- src/hook/useContacts.ts
+++ src/hook/useContacts.ts
@@ -1,6 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
-import { Contact } from "../types/types";
 
 // Hook for handling contacts CRUD operations
 export function useContacts() {
